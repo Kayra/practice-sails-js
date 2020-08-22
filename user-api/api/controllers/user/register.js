96f6d2533541867a4e431f929550c6c57baf1846
Kayra <KayraAlat@gmail.com>
2020-08-22 09:35:00 +0100

    Added user registration business logic

--- user-api/api/controllers/user/register.js
+++ user-api/api/controllers/user/register.js
@@ -31,7 +31,42 @@ module.exports = {
       description: 'An error occurred.'
     },
   },
-  fn: async function(inputs) {
-    return;
+  fn: async function(inputs, exits) {
+    try {
+
+      const newEmailAddress = inputs.email.toLowerCase();
+      const token = await sails.helpers.strings.random('url-friendly');
+
+      let newUser = await User.create({
+        fullName: inputs.fullName,
+        email: newEmailAddress,
+        password: inputs.password,
+        accountProofToken: token,
+        accountProofTokenExpiresAt:
+          Date.now() + sails.config.custom.accountProofTokenTTL,
+      }).fetch();
+
+      const confirmLink = `${sails.config.custom.baseUrl}/user/confirm?token=${token}`;
+      console.log('Confirmation link: ', confirmLink);
+
+      return exits.success({
+        message: `An account has been created for ${newUser.email} successfully. Visit the confirmation link to verify.`,
+      });
+
+    } catch (error) {
+
+      if (error.code === 'E_UNIQUE') {
+        return exits.emailAlreadyInUse({
+          message: 'Error:',
+          error: 'This email address already exits',
+        });
+      } else {
+        return exits.error({
+          message: 'Error:',
+          error: error.message,
+        });
+      }
+
+    }
   }
 };
